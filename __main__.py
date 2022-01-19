371d93ce2d8aca6cc8cffe0826fa967bd21b8253
ubmay <alphonsus.mn@gmail.com>
2022-01-19 13:49:53 +0800

    Renamed the main file to __main__ to make python automatically execute the file.

---
+++ __main__.py
@@ -0,0 +1,10 @@
+print("hello world")
+print("pokemon game timeeee")
+class pokemon:
+    def __init__(self, healthPoints, defense, spDefense, attack, spAttack, moveOne, moveTwo, moveThree):
+    self.healthPoints = healthPoints
+    self.defense = defense
+    self.spDefense = spDefense
+    self.attack = attack
+    self.spAttack = spAttack
+  
