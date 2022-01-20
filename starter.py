6c2a8c3573106aa305c565d0e6706ac8bfff7eee
ubmay <alphonsus.mn@gmail.com>
2022-01-20 15:55:47 +0800

    Kai wanted comments so

--- starter.py
+++ starter.py
@@ -1,4 +1,4 @@
-# Opening
+# Opening print statements
 print("Welcome to the amazing world of Pokémon!")
 print("Choose your starter Pokémon!\n")
 print("""
@@ -9,18 +9,30 @@ The tiny turtle Pokémon, \033[38;5;81m\033[1mSquirtle!\033[0m
 """)
 print("Which will you choose?")
 
-# Must put at top level
+
+
+
+# Some global variables
 pokemon = []
 starter_pokemon_names = ["charmander", "bulbasaur", "squirtle"]
 pokemon_names = ["charmander", "bulbasaur", "squirtle", "pikachu"]
 
 
+
+# For the secret pikachu unlock
 ___starter_py_secret = int(0)
 ___starter_py_secret_cap = 3
 
+
+
+# Returns the first letter uppercase and the rest as lowercase.
 def ___starter_py_capfirst(strig : str):
     return strig[0].upper() + strig[1:].lower()
 
+
+
+# Code that continuously asks for a confirmation.
+# Takes pokemon name as an input.
 def ___starter_py_confirm(_pokename : str):
     pokename = ___starter_py_capfirst(_pokename)
     while (1):
@@ -30,36 +42,41 @@ def ___starter_py_confirm(_pokename : str):
         elif (confirm[0] == "n"):
             return False
         else:
+            # If they didn't answer properly, re-ask for confirmation
             continue
 
+
+# To be run when game first starts.
 def GameLaunchFirstTime():
     global ___starter_py_secret, ___starter_py_secret_cap
     while (1):
         # Special case handling
-        # i have 15 minutes
-        # you see how slowly i code?
-        # i make it gud quality but very slow
+        # If they already collected the secret, break
         got_secret = 0
         if (___starter_py_secret == ___starter_py_secret_cap):
             print("You have activated a secret event!")
+
+            # Continuously ask for confirmation, with different print() values
             while (1):
                 sp_confirm = input("Do you want to claim the alternate starter Pokémon? [y/n] ").lower()
                 if (sp_confirm[0] == "y"):
+                    # They really want the pikachu
+
                     print("Congratulations! You have received the secret \033[38;5;227m\033[1mPikachu!\033[0m")
                     pokemon.append("pikachu")
                     got_secret = 1
                     break
                 elif (sp_confirm[0] == "n"):
+                    # They are an idiot
+
                     print("You will not receive the alternate starter Pokémon.")
                     break
                 else:
+                    # If they didn't answer properly, re-ask for confirmation
                     continue
        
         if (got_secret):
             break
-        # my brain is dying, previously i could code
-        # a html challenge in 30 minutes in primary 5
-
         
 
         # Take input and convert into lowercase
@@ -68,11 +85,13 @@ def GameLaunchFirstTime():
         if (curr_name in starter_pokemon_names):
             confirm = ___starter_py_confirm(curr_name)
             if (confirm):
+                # They want the pokemon they chose
                 print("Congratulations! You have chosen %s!\n" % (pokename))
                 pokemon.append(curr_name)
                 ___starter_py_secret = 0
                 break
             else:
+                # They not confirmed it
                 print("\n")
                 ___starter_py_secret += 1
                 continue
@@ -80,4 +99,6 @@ def GameLaunchFirstTime():
             print("That is not a valid Pokémon!\n")
             continue
 
+# I wrapped it in a function, so i have to call it
+# but you can use it anywhere else
 GameLaunchFirstTime()
