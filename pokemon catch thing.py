e09a1c4ca1cef16f0fd4134801ff6e179947dbf8
LOfangQuanZachary <88233381+LOfangQuanZachary@users.noreply.github.com>
2022-01-20 16:09:48 +0800

    calculates pokemon catch chance

---
+++ pokemon catch thing.py
@@ -0,0 +1,31 @@
+#pokeball is val 1
+#great ball is val 2
+#ultraball is val 3
+
+import random
+
+wildPokemonStatusCondition = False
+wildPokemonHealth = 35
+pokemonBall = 1
+catchChance = 100
+def pokemonCatchChance():
+    wildPokemonStatusCondition = False
+    wildPokemonHealth = 35
+    pokemonBall = 1
+    catchChance = 100
+    if wildPokemonStatusCondition == True:
+        catchChance = catchChance + 10
+    catchChance = catchChance + (pokemonBall*5)
+    catchChance = catchChance - (wildPokemonHealth*1.1 + 5) - 10
+    if catchChance < 0 :
+        print("The pokemon broke free!")
+    elif catchChance > 100 :
+        print("The pokemon has been caught!")
+    else :
+        n = random.randint(0, 100)
+        if n >= catchChance:
+            print("The pokemon has broke free!")
+        elif n < catchChance:
+            print("the pokemon has been caught!")
+    print(catchChance)
+pokemonCatchChance()
